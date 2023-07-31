578e2cc6c7ae6cc2093de8aa558c87533181f596
안수민 <20201052@vision.hoseo.edu>
2023-07-31 15:36:44 +0900

    urlencoded 설정 변경

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ if(process.env.ENODE_ENV=='production'){
   app.use(logger('dev'))
 }
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
