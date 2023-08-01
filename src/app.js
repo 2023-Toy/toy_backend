9b81c0c8fcdb66087145148da38aa3f96efd7a76
안수민 <20201052@vision.hoseo.edu>
2023-08-01 16:30:23 +0900

    develop 브랜치에 올려야함

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ if(process.env.ENODE_ENV=='production'){
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/images')));
 
 app.use('/', loginRouter);
 app.use('/users', usersRouter);
