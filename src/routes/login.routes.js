4895a86ad46cc20312d8cc06e291f93090eb268a
안수민 <20201052@vision.hoseo.edu>
2023-08-01 16:28:34 +0900

    login 경로 수정

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -4,7 +4,7 @@ const loginController = require('../controller/login.controller')
 const uploads = require('../module/multer')
 
 router.get('/login', loginController.getLogin);
-router.post('/login', uploads.upload.single("profile_img"),loginController.postLogin);
+router.post('/login', uploads.profile_upload.single("profile_img"),loginController.postLogin);
 // router.delete('/withdraw', loginController.deleteLogin);
 
 module.exports = router;
