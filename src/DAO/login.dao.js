3f9cb9b1988c2fa20efca28d9aa0ee9f49d5d03a
안수민 <20201052@vision.hoseo.edu>
2023-08-02 13:33:32 +0900

    login API 및 logger 옵션 변경

--- src/DAO/login.dao.js
+++ src/DAO/login.dao.js
@@ -13,11 +13,18 @@ function getLogin(token){
                 )
                 reject("DB ERR")
             }
-            logger.info(
-                'DB success [user]' +
-                '\n \t' + queryData +
-                '\n \t' + db_data
-            )
+            if(!db_data.length){
+                logger.info(
+                    'Not DB [user]' +
+                    '\n \t' + "Not access_token"
+                )
+                db_data = "is_new"
+            }
+            else{
+                logger.info(
+                '로그인 success\t' + db_data[0].user_name + " 성공\n"
+                )
+            }
             resolve(db_data)
         })
     })
@@ -36,12 +43,65 @@ function postLogin(parameter){
                 )
                 reject("DB ERR")
             }
+            logger.info(
+                '회원가입 Success ▶\t' + parameter.name + " 성공\n"
+            )
             resolve(db_data)
+        })
+    })
+}
+
+function deleteLogin(parameter){
+    return new Promise((resolve, reject) => {
+        var queryData = `delete from user where user_id = ${parameter.id}`
+        db.query(queryData, (error, db_data) => {
+            if(error){
+                logger.error(
+                    'DB error [user]'+
+                    '\n \t' + queryData +
+                    '\n \t' + error
+                )
+                reject('DB ERR')
+            }
+            logger.info(
+                '회원 탈퇴 Success ▶\t' + parameter.name + " 성공\n"
+            )
+            resolve(db_data)
+        })
+    })
+}
+
+function findUser(id){
+    return new Promise((resolve, reject) => {
+        var queryData = `select user_name, profile_img from user where user_id = ${id}`
+        db.query(queryData, (error, db_data) => {
+            if(error){
+                logger.error(
+                    'DB error [user]'+
+                    '\n \t' + queryData +
+                    '\n \t' + error
+                )
+                reject('DB ERR')
+            }
+            else if(db_data){
+                logger.error(
+                    '회원 조회 Fail ▶\t user_id : ' + id + " 실패\n"
+                )
+            }
+            else{
+                logger.info(
+                    '회원 조회 Success ▶\t' + db_data[0].user_name + " 성공\n"
+                )
+            }
+            resolve(db_data[0])
+
         })
     })
 }
 
 module.exports = {
     getLogin,
-    postLogin
+    postLogin,
+    deleteLogin,
+    findUser
 }
--- src/service/login.service.js
+++ src/service/login.service.js
@@ -1,4 +1,6 @@
 const loginDao = require('../DAO/login.dao')
+const fs = require("fs")
+const logger = require('../config/logger')
 
 async function getLogin(access_token){
     try{
@@ -32,7 +34,7 @@ async function postLogin(parameter){
                 "Status" : 406
             }
         }
-        const login_data = await loginDao.postLogin(parameter)
+        await loginDao.postLogin(parameter)
         return {
             "Message" : "성공",
             "Status" : 200,
@@ -47,8 +49,56 @@ async function postLogin(parameter){
     }
 }
 
+async function deleteLogin(id){
+    try{
+        if(!id){
+            return {
+                "Message" : "user_id가 없습니다.",
+                "Status" : 406
+            }
+        }
+        const login_data = await loginDao.findUser(id);
+        if(!login_data){
+            return {
+                "Message" : "존재하지 않는 user_id 입니다.",
+                "Status" : 400
+            }
+        }
+        const parameter = {
+            id : id,
+            name : "gkdl"//login_data.user_name
+        }
+        await loginDao.deleteLogin(parameter)
+        if(login_data.profile_img != "profile/default.jpg" || login_data.profile_img != "profile/default.png"){
+            try{
+                logger.info(
+                    'File Success [파일 삭제] ▶\t' + login_data.profile_img + " 삭제 성공\n"
+                )
+                fs.unlinkSync("src/public/images/" + login_data.profile_img)
+            }
+            catch(err){
+                logger.error(
+                    'File Error [파일 삭제] ▶\t' + err + " 실패\n"
+                )
+            }
+        }
+        return {
+            "Message" : "성공",
+            "Status" : 200
+        }
+    }
+    catch(err){
+        return {
+            "Message" : "실패",
+            "Status" : 400,
+            "Error_Message" : err
+        }
+    }
+}
+
 
 module.exports = {
     getLogin,
-    postLogin
+    postLogin,
+    deleteLogin
 }
