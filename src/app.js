789949187be32947ac4f87137299e4df8c033905
안수민 <20201052@vision.hoseo.edu>
2023-08-02 20:25:59 +0900

    app 경로 수정

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public/images')));
 app.use('/', loginRouter);
 app.use('/users', usersRouter);
 app.use('/board', communityRouter);
-app.use('/', dealRouter);
+app.use('/deal', dealRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
