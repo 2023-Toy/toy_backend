1fa1a297aa18e55f4e3e8e7cba6eab84d4eebec8
syuh <seonghee5070@gmail.com>
2023-08-03 13:18:45 +0900

    getlistdeal 추가

--- src/service/deal.service.js
+++ src/service/deal.service.js
@@ -1,6 +1,31 @@
 const dealDao = require('../DAO/deal.dao')
 
-async function postDeal(postDeal_req) {
+//get_list_deal
+async function getlistDeal(getlistDeal_req) {
+    try {
+        if(!getlistDeal_req) {
+            return {
+                "Message" : "search data가 없습니다.",
+                "Status" : 406
+            }
+        }
+        const getlistDeal_data = await dealDao.getlistDeal(getlistDeal_req);
+        return {
+            "Message" : "성공",
+            "Status" : 200,
+            "Data" : getlistDeal_data
+        }
+    } catch (err) {
+        return {
+            "Message" : "실패",
+            "Status" : 400,
+            "Error_Message" : err
+        }
+    }
+}
+
+//post_deal_service
+async function postDeal(postDeal_req, postDeal_img_req) {
     try {
         if(!postDeal_req) {
             return {
@@ -8,7 +33,13 @@ async function postDeal(postDeal_req) {
                 "Status" : 406
             }
         }
-        const postDeal_data = await dealDao.postDeal(postDeal_req);
+        if(!postDeal_img_req) {
+            return {
+                "Message" : "file data가 없습니다.",
+                "Status" : 406
+            }
+        }
+        const postDeal_data = await dealDao.postDeal(postDeal_req, postDeal_img_req);
         return {
             "Message" : "성공",
             "Status" : 200,
@@ -23,6 +54,35 @@ async function postDeal(postDeal_req) {
     }
 }
 
+async function putDeal(putDeal_req, putDeal_img_req) {
+    try {
+        if (!putDeal_req) {
+            return {
+                "Message" : "body data가 없습니다.",
+                "Status" : 406
+            }
+        }
+        if (!putDeal_img_req) {
+            return {
+                "Message" : "file data가 없습니다.",
+                "Status" : 406
+            }
+        }
+        const putDeal_data = await dealDao.putDeal(putDeal_req, putDeal_img_req);
+        return {
+            "Message" : "성공",
+            "Status" : 200,
+            "Data" : putDeal_data
+        }
+    } catch(err) {
+        return {
+            "Message" : "실패",
+            "Status" : 400,
+            "Error_Message" : err
+        }
+    }
+}
+
 module.exports = {
-    postDeal
+    getlistDeal, postDeal, putDeal
 }
